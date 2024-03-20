12d51e18347a34012bcae8e0c66365ad9223c277
BSHkr <tmdgh784@naver.com>
2024-03-20 15:43:03 +0900

    Make run

--- src/main/java/RacingMain.java
+++ src/main/java/RacingMain.java
@@ -1,7 +1,28 @@
-public class RacingMain {
+import domain.Car;
+import domain.RacingGame;
+import domain.Winner;
+import view.InputView;
+import view.OutputView;
 
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.List;
+
+public class RacingMain {
+    public static void main(String[] args) throws IOException {
         // TODO: MVC 패턴을 기반으로 자동차 경주 미션 구현해보기
-        System.out.println("Hello, World!");
+        run();
+    }
+
+    private static void run() throws IOException {
+        final String CARNAMES = InputView.getCarNames();
+        final List<Car> racingCars = RacingGame.generateCars(CARNAMES);
+        final int CNT = InputView.getTryCount();
+
+        RacingGame racingGame = new RacingGame(racingCars);
+        racingGame.startRace(CNT, racingGame);
+
+        Winner winner = new Winner();
+        final List<String> winners = winner.findWinners(racingCars);
+        OutputView.printWinners(winners);
     }
 }
