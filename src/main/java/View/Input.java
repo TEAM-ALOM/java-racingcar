ef9dd5cb23728eae2d6c64585851461630c707ef
ryansewook <ryansewook@naver.com>
2024-03-21 21:00:18 +0900

    commit

--- src/main/java/View/Input.java
+++ src/main/java/View/Input.java
@@ -10,7 +10,7 @@ public class Input {
 
     public Input(){}
 
-    public List<String> readCarNames(){
+    public List<String> readCarNames(){ //차 이름 입력
         Parser parser = new Parser();
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String InputCarNames = scanner.nextLine();
@@ -19,13 +19,10 @@ public class Input {
 
     }
 
-    public Integer readAttemptAmount(){
-        System.out.println("시도할 회수는 몇회인가요?");
+    public Integer readAttemptAmount(){ //시도 횟수 입력
+        System.out.println("시도할 횟수는 몇회인가요?");
         return scanner.nextInt();
     }
 
-    private boolean validateAmount(int AttemptAmount){
-        return AttemptAmount > 0;
-    }
 
 }
