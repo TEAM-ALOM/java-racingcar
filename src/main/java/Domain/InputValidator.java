8e329687889b0e14ad6ffb2b0c6fa1bb0f6c0345
ryansewook <ryansewook@naver.com>
2024-03-23 22:38:29 +0900

    commit

--- src/main/java/Domain/InputValidator.java
+++ src/main/java/Domain/InputValidator.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class InputValidator {
     public InputValidator(){}
+
     /*
     시도 횟수 > 0
 
--- src/main/java/Domain/Parser.java
+++ src/main/java/Domain/Parser.java
@@ -1,16 +1,14 @@
 package Domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
     public Parser(){}
 
     public List<String> stringParserByComma(String input){ //,를 기준으로 리스트 나누기
-        ArrayList<String> Output = new ArrayList<>();
-        for(String list:input.split(",")){
-            Output.add(list);
-        }
+        ArrayList<String> Output = new ArrayList<>(Arrays.asList(input.split(",")));
 
         return Output;
 
