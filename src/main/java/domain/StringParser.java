bd6bc3d8d6284d5cba21d81a82c934ab7f8fc964
SongHyeonJun <hoo5152000@naver.com>
2024-03-24 23:33:24 +0900

    문자열 분리 구현(static 사용)

--- src/main/java/domain/StringParser.java
+++ src/main/java/domain/StringParser.java
@@ -3,8 +3,8 @@ package domain;
 import java.util.List;
 
 public class StringParser {
-    public List<String> SplitByComma(String input){
-        List<String> carNameList = List.of(input.split(","));
-        return carNameList;
+    public static List<String> SplitByComma(String input) {
+        List<String> carNames = List.of(input.split(","));
+        return carNames;
     }
 }
