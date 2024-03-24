9227177694011f53fff3c1b2e75ad667bc8bef9b
SongHyeonJun <hoo5152000@naver.com>
2024-03-24 22:38:33 +0900

    레이싱 진행 출력 테스트 구현

---
+++ src/test/java/RacingTest.java
@@ -0,0 +1,30 @@
+import domain.Car;
+import domain.Cars;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import view.OutputView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RacingTest {
+
+    @DisplayName("레이싱 진행 테스트")  // 차 이름과 현재 포지션이 제대로 출력 되는지 확인
+    @Test
+    public void racingTest() {
+        List<String> carNameList = Arrays.asList("Tico", "Kia", "Matiz", "Ren");
+        Cars cars = new Cars(carNameList);
+
+        List<Car> carList = cars.getCars();
+        int count = 1;
+
+        for(Car car : carList){
+            car.setPosition(count);
+            count++;
+        }
+
+        OutputView outputView = new OutputView();
+
+        outputView.printStatus(carList);
+    }
+}
