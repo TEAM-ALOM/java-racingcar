588c60971bda070e106c45731ffd95b40f7d5188
SongHyeonJun <hoo5152000@naver.com>
2024-03-24 23:34:20 +0900

    레이싱 컨트롤러 구현

--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -1,4 +1,38 @@
 package controller;
 
+import domain.Cars;
+import domain.NumberGenerator;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
 public class RacingController {
+    //NumberGenerator numberGenerator = new NumberGenerator();
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+
+    public void startRacing(){
+        Cars cars;
+        List<String> winners;
+        int count;
+
+        cars = new Cars(inputView.readCarName());
+        count = inputView.readCount();
+
+        for(int i=0;i<count;i++){
+            race(cars);
+        }
+
+        winners = cars.findWinner(cars.findMaxPosition());
+        outputView.printWinner(winners);
+    }
+
+    private void race(Cars cars){
+        cars.moveAllCars();
+        outputView.printStatus(cars.getCars());
+    }
+
+
 }
