8e20d30b3da4d4152a3ea9e3248412758f2f30b9
SongHyeonJun <hoo5152000@naver.com>
2024-03-24 21:02:49 +0900

    inputview 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,22 +8,36 @@ import java.util.Scanner;
 public class InputView {
     private static final String REQUEST_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String REQEUST_ATTEMPT_NUMBER = "시도할 회수는 몇회인가요?";
-
+    private static final String CAR_NAME_LENGTH_ERROR = "차 이름은 5자 이내여야 합니다.";
+    private static final String CAR_NAME_BLANK_ERROR = "차 이름은 공백일 수 없습니다.";
     private Scanner scanner = new Scanner(System.in);
 
     public List<String> readCarName() {
         System.out.println(REQUEST_CAR_NAME);
+
         String name = scanner.nextLine();
+        List<String> carNames = StringParser.SplitByComma(name);
+        validateCarName(carNames);
 
-        List<String> carList = StringParser.SplitByComma(name);
-
-        return carList;
+        return carNames;
     }
 
-    public int readCount(){
+    public int readCount() {
         System.out.println(REQEUST_ATTEMPT_NUMBER);
         int count = scanner.nextInt();
+
         return count;
+    }
+
+    private void validateCarName(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException(carName + CAR_NAME_LENGTH_ERROR);
+            }
+            if (carName.length() < 1) {
+                throw new IllegalArgumentException(carName + CAR_NAME_BLANK_ERROR);
+            }
+        }
     }
 
 }
