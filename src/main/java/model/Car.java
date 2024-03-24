046291a1e31374b13acdf83bec0e8993ed7d378d
강지윤 <23011595@sju.ac.kr>
2024-03-24 23:32:52 +0900

    수정중

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Car {
+    private String _name;
+    private int _move;
+
+    public Car(String name) {
+        _name = name;
+        _move = 0;
+    }
+
+    public void move() {
+
+    }
+}
