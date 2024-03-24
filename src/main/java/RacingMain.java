a2e3048ba0f030d96c58da2372b77f999e9c274f
Jeongyeun <ruthie0219@naver.com>
2024-03-24 23:49:52 +0900

    출력 및 게임 수행 과정(미완성)

--- src/main/java/RacingMain.java
+++ src/main/java/RacingMain.java
@@ -1,7 +1,10 @@
-public class RacingMain {
+import View.InputView;
 
+public class RacingMain {
     public static void main(String[] args) {
-        // TODO: MVC 패턴을 기반으로 자동차 경주 미션 구현해보기
-        System.out.println("Hello, World!");
+
+        final var carNames = InputView.CarNames();
+        final var tryCount = InputView.AttemptNum();
+
     }
 }
