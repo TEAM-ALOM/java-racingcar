a368330a282e6740e18d2c8fb96232f9277a62ba
SongHyeonJun <hoo5152000@naver.com>
2024-03-24 16:33:01 +0900

    차 이름 길이 테스트 임시구현

--- src/test/java/StringParserTest.java
+++ src/test/java/StringParserTest.java
@@ -2,8 +2,11 @@ import domain.StringParser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class StringParserTest {
@@ -19,4 +22,19 @@ public class StringParserTest {
         }
         assertThat(result).containsExactly("orange", "lemon", "apple");
     }
+
+    @DisplayName("차 이름 길이 테스트")
+    @Test
+    void checkCarNameLength(){
+        List<String> carList = Arrays.asList("Tico", "Kia", "Matiz", "Ren");
+        //List<String> carList = Arrays.asList("Ticooo", "Kiaaaa", "Matiz", "Ren");
+
+        //테스트 성공(5자 초과하는 이름이 없을 때), 테스트 실패(5자 초과하는 이름 존재 시)
+        carList.forEach(carName ->
+                assertTrue(carName.length() < 6, carName + "의 길이가 6자 이상.")
+        );
+
+        //추후 리스트를 받아와서 그걸로 테스트하게 변경
+    }
+
 }
