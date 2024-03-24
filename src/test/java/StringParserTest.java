f675ca65f5dd4e82f50199070fb487b932859534
SongHyeonJun <hoo5152000@naver.com>
2024-03-24 16:17:30 +0900

    클래스 이름 수정

--- src/test/java/StringParserTest.java
+++ src/test/java/StringParserTest.java
@@ -1,16 +1,15 @@
 import domain.StringParser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
-public class RacingTest {
+public class StringParserTest {
     @DisplayName("콤마 분리 테스트")
     @Test
-    void splitTest(){
+    void splitTest() {
         //StringParser stringParser = new StringParser();
         String testString = "orange,lemon,apple";
 
@@ -18,7 +17,6 @@ public class RacingTest {
         for (String item : result) {
             System.out.println(item);
         }
-
         assertThat(result).containsExactly("orange", "lemon", "apple");
     }
 }
