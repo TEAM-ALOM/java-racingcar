44ea420eac3f9b81181cf928732a99c6f2166157
nahyunKoo <angelcindy09@gmail.com>
2024-03-25 11:43:15 +0900

    feat(getCarNames) :  경주 자동차 이름 입력 받기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,8 +3,17 @@ package view;
 import java.util.*;
 
 public class InputView {
-    public static Map<String, Integer> getCarNames(){
 
+    private static Scanner scanner = new Scanner(System.in);
+    public static Map<String, Integer> getCarNames(){
+        Map<String, Integer> cars = new HashMap<>();
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String line = scanner.nextLine();
+        String[] str = line.split(",");
+        for(int i = 0; i<str.length; i++){
+            cars.put(str[i], 1);
+        }
+        return cars;
     }
 
     public static int getTryCount(){
