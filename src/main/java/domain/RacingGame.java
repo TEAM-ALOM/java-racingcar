94a84d23f7cab0e3b3937bd822073a9208962e30
nahyunKoo <angelcindy09@gmail.com>
2024-03-25 11:48:23 +0900

    feat(RacingGame) : 생성자

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -3,10 +3,12 @@ package domain;
 import java.util.*;
 
 public class RacingGame {
-
+    private Map<String, Integer> carNames;
+    private int tryCount;
 
     public RacingGame(Map<String, Integer> carNames, int tryCount){
-
+        this.carNames = carNames;
+        this.tryCount = tryCount;
     }
 
     public void race(){
