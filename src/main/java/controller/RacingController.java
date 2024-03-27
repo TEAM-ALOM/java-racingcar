5ff3028fb9ddba4c2d3f35fa26de49044d4693d8
nahyunKoo <angelcindy09@gmail.com>
2024-03-27 10:40:56 +0900

    refactor(generateRandomDistance) : 반복문 구조 변경

--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -4,16 +4,12 @@ import domain.RacingGame;
 import view.InputView;
 import view.ResultView;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 
 public class RacingController {
     public void run(){
         try{
-
             final var cars = InputView.readCarNames();
             final var tryCount = InputView.readTryCount();
             InputView.carNamesValidate(cars);
@@ -22,7 +18,7 @@ public class RacingController {
             final var racingGame = new RacingGame(cars, tryCount);
             race(racingGame);
 
-            ResultView.printWinners(getWinners());
+            ResultView.printWinners(getWinners(racingGame.getCars()));
 
         }catch(IllegalArgumentException e){
             System.out.println(e.getMessage());
@@ -37,17 +33,18 @@ public class RacingController {
     }
 
     private void generateRandomDistance(Map<String, Integer> cars){
-        Iterator<String> iterator = cars.keySet().iterator();
-        while(iterator.hasNext()){
-            String name = iterator.next();
+        for(Map.Entry<String, Integer> entry : cars.entrySet()){
             Random random = new Random();
-            int randomNumber = random.nextInt(10);
-            RacingGame.updateDistance(name, randomNumber);
+            int randomNum = random.nextInt(10);
+            RacingGame.updateDistance(entry.getKey(), randomNum);
         }
     }
 
-    private List<String> getWinners(){
+    private List<String> getWinners(Map<String, Integer> cars){
+        List<String> winners = new ArrayList<>();
 
+
+        return winners;
     }
 
 }
