459111ae33d7239e0c0a7ad4c9a9e1e7b913c26b
SongHyeonJun <hoo5152000@naver.com>
2024-03-28 18:55:09 +0900

    moveCar 테스트

--- src/test/java/RacingTest.java
+++ src/test/java/RacingTest.java
@@ -1,11 +1,15 @@
 import domain.Car;
 import domain.Cars;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import view.OutputView;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RacingTest {
 
@@ -24,7 +28,22 @@ public class RacingTest {
         }
 
         OutputView outputView = new OutputView();
-
         outputView.printStatus(carList);
+    }
+    @Test
+    @DisplayName("차 이동 성공 테스트")
+    void moveCarTestSuccess(){
+        Car car = new Car("superCar");
+        car.setPosition(2);
+        car.move(6);
+        assertThat(car.getPosition()).isEqualTo(3);
+    }
+    @Test
+    @DisplayName("차 이동 실패 테스트")
+    void moveCarTestFalied(){
+        Car car = new Car("superCar");
+        car.setPosition(2);
+        car.move(1);
+        assertThat(car.getPosition()).isEqualTo(3);  //차가 이동하지 못해 에러 발생시키기
     }
 }
