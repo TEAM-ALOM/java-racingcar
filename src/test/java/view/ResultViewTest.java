cd2fb7e682fd921212a0100c427826055eb2deaf
nahyunKoo <angelcindy09@gmail.com>
2024-03-28 16:25:30 +0900

    test : 테스트 클래스 추가

---
+++ src/test/java/view/ResultViewTest.java
@@ -0,0 +1,18 @@
+package view;
+
+import org.junit.jupiter.api.Test;
+import org.assertj.*;
+
+public class ResultViewTest {
+
+    @Test
+    void 기능_테스트(){
+        //given
+
+        //when
+
+        //then
+
+
+    }
+}
